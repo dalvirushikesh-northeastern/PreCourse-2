61e5a542802634699421f029a1683b1d9128458b
Rushikesh Dalvi <rushikeshdalvi22@gmail.com>
2024-12-18 00:14:35 -0500

    precourse-2 exe-1,exe-2 done

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,12 +3,25 @@
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
+
+# Time Complexity = O(logn)
+# Space Complexity = O(1)
+# Any problem you faced while coding this : No
+# 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
-  
+   while l <= r:
+       # Calculating middle position
+       mid = l + (r -l) //2 
+       # If value we are looking for is at mid we will return it
+       if arr[mid] == x:
+           return mid
+       # if mid is greater than x shift right pointer to mid -1
+       elif arr[mid] > x:
+           r = mid - 1
+       else:
+           l = mid + 1
+   # returning -1 if value is not present in sorted array
+   return -1
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +30,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index % d" % result)
 else: 
-    print "Element is not present in array"
+    print ("Element is not present in array")
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,17 +1,33 @@
-# Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
+# Time Complexity: O(n log n) 
+# Space Complexity: O(log n) due to the recursion stack 
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this :choosing the the right pivot and handling edge case for duplicate element in the array
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
-  
+
+def partition(arr,low,high):
+    pivot = arr[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+        # If the current element is smaller than or equal to pivot
+        if arr[j] <= pivot:
+            i += 1  # Increment the index of the smaller element
+            arr[i], arr[j] = arr[j], arr[i]  # Swap
+
+    # Swap the pivot element with the element at i+1
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1  # Return the partitioning index
+
+def quickSort(arr, low, high):
+    if low < high:
+        # Partition the array and get the partition index
+        pi = partition(arr, low, high)
+
+        # Recursively sort elements before and after the partition
+        quickSort(arr, low, pi - 1)  # Left subarray
+        quickSort(arr, pi + 1, high)  # Right subarray
+
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
